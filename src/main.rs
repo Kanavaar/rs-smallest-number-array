755a6ca8b8a8798c143b6b3c8647e766e5f5d6b6
Kanavaar <uju8765@gmail.com>
2023-02-08 17:42:18 +0100

    removing unused code

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,3 @@
-//const TEST: u32 = 60 * 60 *3;
-//
-//fn is_even(num: i64) -> bool {
-//    match num % 2 {
-//        0 => true,
-//        _ => false,
-//    }
-//}
-//
-//fn tuples() {
-//    let tup: (i32, f64, &str) = (69, 42.0, "Ich liebe mich");
-//    let (tup_num_one, tup_float, a_string) = tup;
-//    let a = tup.0;
-//    let b = tup.1;
-//    let c = tup.2;
-//    println!("{}{}{}{}{}{}", tup_num_one, tup_float, a_string, a, b, c);
-//}
-//
-//fn arrays() {
-//    let a: [i32; 5] = [1, 2, 3, 4, 5];
-//    let array_pos_0 = a[0];
-//    println!("{array_pos_0}");
-//}
-//
-//
-
 use rand::Rng;
 
 fn smallest_num_array(arr: [i64; 10]) -> i64 {
@@ -43,17 +17,6 @@ fn smallest_num_array(arr: [i64; 10]) -> i64 {
 }
 
 fn main() {
-    //println!("{TEST}");
-    //let answer: isize = "42".trim().parse().expect("Not a number");
-    //println!("{answer}");
-    //for i in 0..=16 {
-    //    if is_even(i) == true {
-    //        println!("{i}");
-    //    }
-    //}
-    //tuples();
-    //arrays();
-
     let a = rand::thread_rng().gen_range(1..=100);
     let b = rand::thread_rng().gen_range(1..=100);
     let c = rand::thread_rng().gen_range(1..=100);
